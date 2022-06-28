d6fe11202a56289a036f7aa3b36f8ed8fbddd726
jtm526 <jitae.moon@outlook.com>
2022-06-28 22:20:59 +0900

    Create triple_pointer.c

---
+++ Chapter17/triple_pointer.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int num = 100;
+	int* ptr = &num;
+	int** dptr = &ptr;
+	int*** tptr = &dptr; // triple pointer에는 douple pointer의 주소값 저장
+
+	printf("%d %d %d\n", *ptr, **dptr, ***tptr);
+
+	return 0;
+}
