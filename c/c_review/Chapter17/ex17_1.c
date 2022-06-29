8e9db28beb04b106df6afca793aebf6eee15f085
jtm526 <jitae.moon@outlook.com>
2022-06-29 19:13:50 +0900

    Change path

---
+++ c/c_review/Chapter17/ex17_1.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+
+void max_and_min(); // 가장 큰 값, 가장 작은 값이 저장된 배열의 주소값 저장
+
+int main(void)
+{
+	int* max_ptr;
+	int* min_ptr;
+	int arr[5] = { 1, 2, 3, 4, 5 };
+}
+
+void max_and_min();
