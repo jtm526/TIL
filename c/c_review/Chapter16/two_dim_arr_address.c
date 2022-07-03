a10d2da79f5fe8c7f0e8d1b7fe993379f748d6ff
jtm526 <jitae.moon@outlook.com>
2022-07-03 22:57:23 +0900

    Create src file
    chapter 16 review

---
+++ c/c_review/Chapter16/two_dim_arr_address.c
@@ -0,0 +1,16 @@
+﻿// 2차원 배열 주소값 알아보기
+#include <stdio.h>
+
+int main(void)
+{
+	int arr[3][2];
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%p\n", &arr[i][j]);
+		}
+	}
+
+	return 0;
+}
---
+++ c/c_review/Chapter16/two_dim_array_size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int arr1[3][4];
+	int arr2[7][9];
+	printf("sizeof arr1 is %zu\n", sizeof(arr1));
+	printf("sizeof arr2 is %zu\n", sizeof(arr2));
+
+	return 0;
+}
